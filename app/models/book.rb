d2aeb9aa26cb3897df2cf446b142c97f42c43d54
ninunu7 <pibf51@gmail.com>
2021-04-06 08:23:31 +0000

    a

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,20 +8,20 @@ class Book < ApplicationRecord
 
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
-	
+
 	def favorited_by?(user)
      favorites.where(user_id: user.id).exists?
 	end
 
 	def self.search(search,word)
-　　if search == "forward_match"
-                　 @book = Book.where("name LIKE?","#{word}%")
-　　elsif search == "backward_match"
-                　　@book = Book.where("name LIKE?","%#{word}")
-　　elsif search == "perfect_match"
-                　　@book = Book.where("#{word}")
-　　elsif search == "partial_match"
-                　　@book = Book.where("name LIKE?","%#{word}%")
-
-  end
+		if search == "forward_match"
+	                  @book = Book.where("title LIKE?","#{word}%")
+		elsif search == "backward_match"
+	                  @book = Book.where("title LIKE?","%#{word}")
+		elsif search == "perfect_match"
+	                  @book = Book.where("#{word}")
+		elsif search == "partial_match"
+                  @book = Book.where("title LIKE?","%#{word}%")
+		end
+	end
 end
